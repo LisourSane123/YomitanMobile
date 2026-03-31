4080f1e223fe06e272902c50b20e282dcbbb8cc2
Lisour_sane123 <l.swietochow@student.uw.edu.pl>
2026-03-31 16:19:04 +0200

    security cleanup and disabled adb backup

--- fix_kanji_html.py
+++
@@ -1,9 +0,0 @@
-with open("app/src/main/java/com/yomitanmobile/data/anki/AnkiCardCreator.kt", "r") as f:
-    lines = f.readlines()
-
-for i, line in enumerate(lines):
-    if "kanji.meanings.isNotEmpty" in line:
-        lines[i] = '                (if (kanji.meanings.isNotEmpty()) "<br>Znaczenie: " + kanji.meanings else "") +\n'
-
-with open("app/src/main/java/com/yomitanmobile/data/anki/AnkiCardCreator.kt", "w") as f:
-    f.writelines(lines)
